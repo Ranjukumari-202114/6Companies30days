4780740b86de19b978cc78f52b0f233a501167de
Ranju kumari <kumariranju1606@gmail.com>
2023-01-13 16:15:21 +0530

    program

---
+++ day12.java (7)/Main.java
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+class Solution {
+    public int findMin(int[] nums) {
+     int left = 0;
+      int right = nums.length-1;
+      while(left<right) 
+      {
+          int mid = (left+right)/2;
+          if(nums[mid]> nums[right])
+          {
+              left = mid+1;
+              
+          }
+          else if(nums[mid]<nums[right])
+          {
+              right = mid;
+          }
+          else
+          {
+              right--;
+          }
+
+      } 
+      return nums[left];   
+    }
+}
