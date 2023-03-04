28278b8fef4d16e6a6bf75d87579cdbda118f6e4
Ranju kumari 202114 <91342000+Ranjukumari-202114@users.noreply.github.com>
2023-03-04 22:37:41 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+
+//1025. Divisor Game
+
+
+
+class Solution {
+    public boolean divisorGame(int n) {
+      
+     if(n%2==1)
+     {
+         return false;
+     } 
+     return true;   
+    }
+  
+}
