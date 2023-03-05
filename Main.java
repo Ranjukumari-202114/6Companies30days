3274b32710d9faa7caeb5c8c06c07771f0eeb22f
Ranju kumari 202114 <91342000+Ranjukumari-202114@users.noreply.github.com>
2023-03-05 22:28:24 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -7,18 +7,50 @@ Write your code in this editor and press "Run" button to execute it.
 *******************************************************************************/
 
 
-//1025. Divisor Game
+//1232. Check If It Is a Straight Line
 
 
 
 class Solution {
-    public boolean divisorGame(int n) {
-      
-     if(n%2==1)
+    public boolean checkStraightLine(int[][] coordinates) {
+    
+    /*
+    //wrong  ans
+
+     double slope = Calculate(coordinates[0],coordinates[1]);
+     for(int i =2; i<coordinates.length;i++)
      {
-         return false;
+         if(slope !=Calculate(coordinates[0],coordinates[i]))
+         {
+             return false;
+         }
+         
      } 
-     return true;   
+     return true;
+     }
+
+     private double Calculate(int[] p1, int[] p2)
+     {
+         return ((p1[1]-p2[1])*1.0)/((p1[0]-p2[0])*1.0);
+     } 
+
+  */
+
+    int x0 = coordinates[0][0];
+    int y0 = coordinates[0][1];
+    int x1 = coordinates[1][0];
+    int y1 = coordinates[1][1];
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+
+    for (int i = 2; i < coordinates.length; ++i) {
+      int x = coordinates[i][0];
+      int y = coordinates[i][1];
+      if ((x - x0) * dy != (y - y0) * dx)
+        return false;
     }
-  
+
+    return true; 
+    }
+    
 }
