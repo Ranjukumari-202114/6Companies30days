ef56560ef49b03f1d6d0f4fd15977c403994ec45
Ranju kumari <kumariranju1606@gmail.com>
2023-04-13 23:28:53 +0530

    program

---
+++ day96.java/Main.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+
+//709. To Lower Case
+
+
+
+class Solution {
+    public String toLowerCase(String s) {
+       return s.toLowerCase(); 
+    }
+}
