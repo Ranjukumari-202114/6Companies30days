a51f0d699ee31531f268268a11b13d9c324fd2e7
Ranju kumari <kumariranju1606@gmail.com>
2023-04-21 23:01:36 +0530

    program

---
+++ day104.java/Main.java
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+
+//461. Hamming Distance
+
+
+
+
+class Solution {
+    public int hammingDistance(int x, int y) {
+      int xor = x^y;
+        int count = 0;
+        while (xor > 0) {
+            if ((xor&1) == 1) {
+                count ++;
+            }
+            xor = xor >> 1;
+        }
+        return count;   
+    }
+}
