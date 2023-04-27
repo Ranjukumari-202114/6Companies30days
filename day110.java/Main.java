7f9ed900f1c1a5f310e60b65b866e51610e4412a
Ranju kumari <kumariranju1606@gmail.com>
2023-04-27 22:31:26 +0530

    program

---
+++ day110.java/Main.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+
+//319. Bulb Switcher
+
+
+
+class Solution {
+    public int bulbSwitch(int n) {
+     return (int) Math.sqrt(n);   
+    }
+}
